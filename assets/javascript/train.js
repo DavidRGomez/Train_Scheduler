6415c658d3ce86eca3c332598002a0dd8aa03761
DavidRGomez'git config --global user.email gomez_inc <gomez_inc@ymail.com>
2017-03-12 12:55:00 -0500

    upload

---
+++ assets/javascript/train.js
@@ -0,0 +1,28 @@
+
+
+
+  	var firebase = firebase.database();
+  	var trainName = "";
+  	var destination = "";
+  	var freq = "";
+  	var nextTrainArrival = "";
+  	var minAway ="";
+  	
+
+  	// firebase.set({
+  	// 	trainName: trainName,
+  	// 	destination: destination,
+  	// 	nextTrainArrival: nextTrainArrival,
+  	// 	freq: freq,
+  	// 	minAway: minAway
+  	// });
+
+  	$('#submitTrainInfo').on("click", function(){
+  		
+  		console.log(trainName, destination, freq);
+
+  		
+
+  	})
+
+
